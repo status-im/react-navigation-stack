2f1f8cd782ffcba362ae333e3dc630e237e97669
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2019-02-25 13:55:51 +0200

    remove background from StackViewCard

--- src/views/StackView/StackViewCard.js
+++ src/views/StackView/StackViewCard.js
@@ -48,7 +48,7 @@ class Card extends React.Component {
 const styles = StyleSheet.create({
   main: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: '#E9E9EF',
+    //backgroundColor: '#E9E9EF',
     shadowColor: 'black',
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.2,
